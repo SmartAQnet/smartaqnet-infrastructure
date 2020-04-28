17d76b487061c8781e395aace0c5d363606cf8b8
Leon Kaucher <kaucher@teco.edu>
2020-04-28 13:57:29 +0200

    Comments in deploy_all_prod.sh

--- deploy_all_prod.sh
+++ deploy_all_prod.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
+#Export HOST_NAME for use in Traefik and Frost. Frost uses this to determine links (e.g. nextlinks)
+#Traefik uses this for routes
 export HOST_NAME=api.smartaq.net
 
 # ------ FAAS Options ---------
+#Deactivates basic auth in the FAAS gateway. The Gateway is behind Traefik and traefik handles auth.
 export BASIC_AUTH=false
 echo "" | docker secret create basic-auth-password -
 echo "" | docker secret create basic-auth-user -
 # ------ FAAS Options End ---------
 
+#Deploy the Traefik TCP & HTTP reverse proxy first
 docker stack deploy -c ./traefik/docker-compose.yml traefik
-#docker stack deploy -c ./registry/docker-compose.yml registry
 
-#sleep 15
-
+#Build the open source image of timescaleDB postgis. The official image is under the timescale license. This image is built from the Apache 2.0 licensed core and adds the postgis extension.
 cd postgis
 ./deploy_timescaledb-oss.sh
 cd ..
 
+#Build and push postgis and netdata to the TECO docker registry
 docker-compose -f ./postgis/docker-compose.yml build
 docker-compose -f ./postgis/docker-compose.yml push
 docker-compose -f ./netdata/docker-compose.yml build
 docker-compose -f ./netdata/docker-compose.yml push
 
+#Deploys all stacks to the cluster
 docker stack deploy --with-registry-auth -c ./postgis/docker-compose.yml postgis
 docker stack deploy -c ./frost/docker-compose.yml frost
-
 docker stack deploy -c ./faas/docker-compose.yml -c ./faas/docker-compose.traefik.yml faas
 docker stack deploy --with-registry-auth -c ./netdata/docker-compose.yml netdata
 docker stack deploy -c ./portainer/docker-compose.yml portainer
